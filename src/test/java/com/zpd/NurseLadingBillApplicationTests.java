ce6b979af0a4a054fe19ee8ea2251e7b403cc7ce
root <root@localhost.zpd.com>
2019-09-02 15:22:50 +0800

    woyourenxingle

---
+++ src/test/java/com/zpd/NurseLadingBillApplicationTests.java
@@ -0,0 +1,16 @@
+package com.zpd;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class NurseLadingBillApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
