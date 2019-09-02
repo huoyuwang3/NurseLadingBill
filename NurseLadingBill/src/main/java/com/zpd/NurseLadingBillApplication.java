26fb10c2a56c7c42dd8c779707869106a59aac52
root <root@localhost.zpd.com>
2019-09-02 15:17:37 +0800

    worenxing

---
+++ NurseLadingBill/src/main/java/com/zpd/NurseLadingBillApplication.java
@@ -0,0 +1,13 @@
+package com.zpd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NurseLadingBillApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NurseLadingBillApplication.class, args);
+	}
+
+}
